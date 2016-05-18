59edda7ce0e8f7a06f6026a96a01fc6a721a84a6
lxw0109 <lxw0109@gmail.com>
2016-05-18 17:11:12 +0800

    [sync] lxw electron first Demo

---
+++ interact.js
@@ -0,0 +1,5 @@
+var value = 10;
+function func(){
+	return "hello lxw in .js file";
+}
+document.write(func());
---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello{
+    public Hello(){}
+    public static String sayHelloStatic(){
+        return "lxw: Hello in sayHelloStatic()";
+    }
+    public String sayHello(){
+        return "lxw: Hello in sayHello()";
+        //System.out.println("Hello in sayHello1()");
+    }
+}
